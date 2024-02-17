8bde9bde8a6f0221f48fbfc3da47009123e97f29
bhanuprk <160158091+bhanuprk@users.noreply.github.com>
2024-02-17 13:16:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "my_vm" {
  ami           = var.ami //Ubuntu AMI
  instance_type = var.instance_type
- vpc_id = "vpc-0f04b54b444d62eb3"
  subnet_id = "subnet-0b1e0e94398be7a67"
 
  tags = {
